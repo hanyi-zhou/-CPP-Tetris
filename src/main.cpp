d5e4300e99da89e2355cb531f2bcf9f4e65af351
Hanna Zhou <zhou.hanyi@northeastern.edu>
2023-10-10 23:53:50 -0700

    create assets and implement rotate function

---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+wstring tetromino[7];
+
+int Rotate(int px, int py, int r)
+{
+    switch (r % 4)
+    {
+        case 0: return py * 4 + px;          // 0 degree
+        case 1: return 12 + py - (px * 4);   // 90 degrees
+        case 2: return 15 - (py * 4) - px;   // 180 degrees
+        case 3: return 3 - py + (px * 4);    // 270 degrees
+    }
+    return 0;
+}
+
+int main()
+{
+    // Create assets
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+
+    tetromino[1].append(L"..X.");
+    tetromino[1].append(L".XX.");
+    tetromino[1].append(L".X..");
+    tetromino[1].append(L"....");
+
+    tetromino[2].append(L".X..");
+    tetromino[2].append(L".XX.");
+    tetromino[2].append(L"..X.");
+    tetromino[2].append(L"....");
+
+    tetromino[3].append(L"....");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L"....");
+
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L".XX.");
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L"....");
+
+    tetromino[5].append(L"....");
+    tetromino[5].append(L".XX.");
+    tetromino[5].append(L"..X.");
+    tetromino[5].append(L"..X.");
+
+    tetromino[6].append(L"....");
+    tetromino[6].append(L".XX.");
+    tetromino[6].append(L".X..");
+    tetromino[6].append(L".X..");
+
+    
+
+    return 0;
+}
